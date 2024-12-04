1175ad821aea6ee6a9ef0b9d551d33ab5630c726
farisfian06 <farisfian06@gmail.com>
2024-12-05 00:29:56 +0700

    fixxing detail artikel

--- app/src/main/java/com/example/readmate/DetailArtikel.java
+++ app/src/main/java/com/example/readmate/DetailArtikel.java
@@ -2,6 +2,8 @@ package com.example.readmate;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -10,8 +12,11 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 import java.util.concurrent.atomic.AtomicMarkableReference;
@@ -22,10 +27,14 @@ public class DetailArtikel extends AppCompatActivity implements View.OnClickList
     private TextView tvTanggal;
     private TextView tvIsi;
     private ImageView ivThumbnail;
-
     private Artikel artikel;
     private FirebaseDatabase db;
-    private DatabaseReference appdb;
+    private DatabaseReference appdbBookmark;
+    private DatabaseReference appdbComment;
+    private Comment komentar;
+    private TextView tvNama;
+    private TextView tvIsiKomentar;
+    private ImageView ivKomentar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +44,10 @@ public class DetailArtikel extends AppCompatActivity implements View.OnClickList
         ImageButton backBtn = findViewById(R.id.backBtn);
         ImageButton bookmarkBtn = findViewById(R.id.bookmarkBtn);
         ImageButton commentBtn = findViewById(R.id.commentBtn);
+        
+        this.tvNama = findViewById(R.id.tvNama);
+        this.tvIsiKomentar = findViewById(R.id.tvIsiKomentar);
+        this.ivKomentar = findViewById(R.id.ivKomentar);
 
         this.artikel = (Artikel) getIntent().getSerializableExtra("artikel");
 
@@ -44,15 +57,15 @@ public class DetailArtikel extends AppCompatActivity implements View.OnClickList
         this.tvIsi = findViewById(R.id.tvIsiArtikel);
         String isiArtikel = artikel.getIsi();
         if (isiArtikel!= null && !isiArtikel.isEmpty()){
-        isiArtikel = isiArtikel.replace("\\n", "\n");
+            isiArtikel = isiArtikel.replace("\\n", "\n");
         }
+
         this.tvIsi.setText(isiArtikel);
         this.tvTanggal.setText(artikel.getTanggal());
         this.tvJudul.setText(artikel.getJudul());
-
         if (artikel.getThumbnail() != null && !artikel.getThumbnail().isEmpty()) {
             Picasso.get()
-                    .load(artikel.getThumbnail()) // asumsi `getProfileUrl` mengembalikan URL gambar profil
+                    .load(artikel.getThumbnail())
                     .placeholder(R.drawable.thumbnail1) // Gambar sementara saat loading
                     .error(R.drawable.ic_launcher_background)
                     .into(this.ivThumbnail);
@@ -61,7 +74,49 @@ public class DetailArtikel extends AppCompatActivity implements View.OnClickList
         String firebaseUrl = "https://readmate-37771-default-rtdb.asia-southeast1.firebasedatabase.app/";
 
         this.db = FirebaseDatabase.getInstance(firebaseUrl);
-        this.appdb = this.db.getReference("bookmarks");
+        this.appdbBookmark = this.db.getReference("bookmarks");
+        this.appdbComment = this.db.getReference("comments");
+
+        this.appdbComment.orderByChild("artikelId").equalTo(artikel.getId())
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.exists()) {
+                            DataSnapshot firstSnapshot = snapshot.getChildren().iterator().next();
+                            Comment c = firstSnapshot.getValue(Comment.class);
+                            Log.e("komen" ,c.getIsi());
+                            komentar = c;
+                            if (c != null) {
+                                // Atur nilai komentar ke TextView setelah data tersedia
+                                tvIsiKomentar.setText(c.getIsi());
+                                tvNama.setText(c.getNama());
+
+                                if (c.getProfile() != null && !c.getProfile().isEmpty()) {
+                                    Picasso.get()
+                                            .load(c.getProfile())
+                                            .placeholder(R.drawable.profile1) // Gambar sementara saat loading
+                                            .error(R.drawable.ic_launcher_background)
+                                            .into(ivKomentar);
+                                }
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) { }
+                });
+
+//        if (komentar!= null){
+//            this.tvIsiKomentar.setText(komentar.getIsi());
+//            this.tvNama.setText(komentar.getNama());
+//            if (komentar.getProfile() != null && !komentar.getProfile().isEmpty()) {
+//                Picasso.get()
+//                        .load(komentar.getProfile())
+//                        .placeholder(R.drawable.profile1) // Gambar sementara saat loading
+//                        .error(R.drawable.ic_launcher_background)
+//                        .into(this.ivKomentar);
+//            }
+//        }
 
         bookmarkBtn.setOnClickListener(this);
 
@@ -70,6 +125,7 @@ public class DetailArtikel extends AppCompatActivity implements View.OnClickList
             public void onClick(View v) {
                 // Intent untuk membuka CommentActivity
                 Intent intent = new Intent(DetailArtikel.this, CommentActivity.class);
+                intent.putExtra("artikel", artikel);
                 startActivity(intent);  // Memulai aktivitas baru
             }
         });
@@ -85,11 +141,11 @@ public class DetailArtikel extends AppCompatActivity implements View.OnClickList
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.bookmarkBtn) {
-         String id = this.appdb.push().getKey();
+         String id = this.appdbBookmark.push().getKey();
             Bookmark bookmark = new Bookmark(id, artikel);
 
             // Simpan bookmark ke Firebase
-            this.appdb.child(id).setValue(bookmark)
+            this.appdbBookmark.child(id).setValue(bookmark)
                     .addOnSuccessListener(aVoid -> {
                         // Tampilkan pesan sukses
                         Toast.makeText(DetailArtikel.this, "Artikel berhasil ditambahkan ke bookmark", Toast.LENGTH_SHORT).show();
